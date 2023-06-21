80556d2c82c4a735873526022c87cd9b154c89f5
ALEXEY ABRAMOV <alexey.abramov96@gmail.com>
2023-06-21 11:35:06 +0300

    Updates

---
+++ assets/preview-3afc2d04.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-818396e2.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-295d31ef.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-295d31ef.js","./iframe-818396e2.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-3afc2d04.js.map
