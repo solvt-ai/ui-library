24c223937b5335b52be46c0d3a827124f22393e5
ALEXEY ABRAMOV <alexey.abramov96@gmail.com>
2023-07-09 01:50:37 +0300

    Updates

---
+++ assets/Button.stories-b774b72e.js
@@ -0,0 +1,5 @@
+import{j as l}from"./index-e9bf0e58.js";import{B as a,a as e,b as o}from"./Button-ffb73469.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const B={title:"components/Button",component:a,argTypes:{intent:{options:[e.Primary,e.Outline],control:{type:"radio"}},size:{options:[o.Small,o.Medium,o.Large],control:{type:"select"}},disabled:{control:{type:"boolean"}},fluid:{control:{type:"boolean"}}},args:{text:"Test button",intent:e.Primary,size:o.Medium,disabled:!1,fluid:!1}},t=({text:i,...p})=>l(a,{text:i,...p});t.storyName="Button";var r,n,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`({
+  text,
+  ...args
+}: ButtonProps) => <Button text={text} {...args} />`,...(s=(n=t.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const y=["ButtonStoryTemplate"];export{t as ButtonStoryTemplate,y as __namedExportsOrder,B as default};
+//# sourceMappingURL=Button.stories-b774b72e.js.map
---
+++ assets/preview-28fd463e.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-a1c322a9.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-89eff3bf.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-89eff3bf.js","./iframe-a1c322a9.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-63c5d83d.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-28fd463e.js.map
