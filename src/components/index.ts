cd6bd0f8db345b2de1edddebbafb81ced94b4901
ALEXEY ABRAMOV <alexey.abramov96@gmail.com>
2023-07-09 23:26:38 +0300

    feat: add imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { Button, ButtonProps, ButtonSize, ButtonIntent } from "./Button";
+
+export { Input, InputProps, InputSize, InputType, IconPosition } from "./Input";
+
+export { Modal, ModalProps, ModalSize } from "./Modal";
